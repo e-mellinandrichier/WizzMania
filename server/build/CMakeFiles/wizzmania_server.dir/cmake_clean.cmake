8da962752992c0d1cca645096fa1acb2e2992250
Emmanuelle Mellinand-Richier <melli@MacBook-Air-de-Emmanuelle.local>
2026-01-23 14:48:16 +0100

    feat: working server made with crow. routes for register/login/list all users.

---
+++ server/build/CMakeFiles/wizzmania_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wizzmania_server.dir/main.cpp.o"
+  "CMakeFiles/wizzmania_server.dir/main.cpp.o.d"
+  "wizzmania_server"
+  "wizzmania_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wizzmania_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server/build/CMakeFiles/wizzmania_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for wizzmania_server.
+# This may be replaced when dependencies are built.
