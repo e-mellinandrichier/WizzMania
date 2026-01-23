03d4722a8591807d55405c79bdd99e92820a5663
leila-wilde <leila.wilde@laplateforme.io>
2026-01-23 15:09:09 +0000

    feat: add handle_client function to connect to inividual clients, read their messages and broadcast them

--- src/server.cpp
+++ src/server.cpp
@@ -13,7 +13,37 @@
 // container to track active client connections
 std::vector<int> clients;
 
-void handle_client(int client_fd) {}
+void broadcast_msg(const std::string& msg, int sender) {}
+
+// handle individual client connection
+void handle_client(int client_fd) 
+{
+    char buffer[1024]; // buffer to store received messages
+    
+    // main communication loop for this client
+    while(true) 
+    {
+        // read data from the client
+        int msg_length = read(client_fd, buffer, sizeof(buffer) -1);
+        
+        // check for connection closure (0) or read error (-1)
+        if(msg_length <= 0) 
+        {
+            break;
+        }
+        
+        // null-terminate the buffer to create valid string
+        buffer[msg_length] = '\0';
+
+        // create a message with client ID & their message
+        std::string msg = "CLIENT " + std::to_string(client_fd) + ": " + buffer;
+
+        broadcast_msg(msg, client_fd);
+    }
+
+    // close the client socket
+    close(client_fd);
+}
 
 int main () 
 {
@@ -38,16 +68,17 @@ int main ()
     // start listening for incoming connections
     listen(server_fd, 10); // backlog 10 max pending connections
 
+    std::cout << "server listening on port " << PORT << std::endl;
+
     // continuous loop to accept client connections
     while(true)
     {
         // accept incoming client connection
+        // return a new socket descriptor for this specific client 
         int client_fd = accept(server_fd, nullptr, nullptr);
         
         // store client file descriptor
-        {
-            clients.push_back(client_fd);
-        }
+        clients.push_back(client_fd);
         
         // create new thread for each client connection
         std::thread(handle_client, client_fd).detach(); // detach allows thread to run independently
